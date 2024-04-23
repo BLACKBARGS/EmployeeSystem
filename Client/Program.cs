a2e8e364aef1893d2547a6b319558576d8893499
Francys Rodrigues <blackgsgofsr@gmail.com>
2024-04-23 08:01:31 -0300

    Updated department components and refactored project structure
    In this commit, several changes were made to the department components and the project structure. The `GeneralDepartmentDialog.razor` file was moved to the `None` item group in `Client.csproj` to prevent it from being included in the build process by default. The `HomePage.razor` was updated to include the `GeneralDepartmentPage` component.
    
    `GeneralDepartmentPage.razor` was enhanced with a new HTML structure for displaying a department list and a `GeneralDepartmentDialog` component. The `IDisposable` interface was implemented to unsubscribe from events on component disposal.
    
    In `Program.cs`, the Syncfusion license registration was removed and the `Syncfusion.Licensing.SyncfusionLicenseProvider` namespace was replaced with `Syncfusion.Licensing`.
    
    Two new DTO classes were created in `BaseDepartmentDTO.cs` and `GeneralDepartmentDTO.cs` to represent department data.
    
    Finally, a new dialog component was created in `GeneralDepartmentDialog.razor` using Syncfusion Blazor components for adding and validating new departments.

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,35 +9,26 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Syncfusion.Blazor;
 using Syncfusion.Blazor.Popups;
-using static Syncfusion.Licensing.SyncfusionLicenseProvider;
+using Syncfusion.Licensing;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-// Registering the custom http handler
 builder.Services.AddTransient<CustomHttpHandler>();
-
 builder.Services.AddHttpClient("SystemApiClient", client =>
 {
     client.BaseAddress = new Uri("https://localhost:7294");
 }).AddHttpMessageHandler<CustomHttpHandler>();
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7294") });
 builder.Services.AddAuthorizationCore();
-
-// Registering the Blazored local storage
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<GetHttpClient>();
 builder.Services.AddScoped<LocalStorageService>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<IUserAccountService, UserAccountService>();
-
 builder.Services.AddScoped<DepartmentState>();
+//License
 
-
-// Registering the Syncfusion dialog service
-string? syncfusionLicenseKey = Environment.GetEnvironmentVariable("SYNCFUSION_LICENSE_KEY");
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(syncfusionLicenseKey);
 builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped<SfDialogService>();
-// Registering the custom authentication state provider
 await builder.Build().RunAsync();
